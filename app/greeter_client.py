298341050b11081cd75a5c794849b4dee56cfb77
zhixian <zhixian@hotmail.com>
2022-12-01 23:11:39 +0800

    update

--- app/greeter_client.py
+++ app/greeter_client.py
@@ -9,18 +9,87 @@ import grpc
 from protos import helloworld_pb2
 from protos import helloworld_pb2_grpc
 
+import protos.basic_example_pb2_grpc as my_grpc
+import protos.basic_example_pb2 as my_pb
+
+log = logging.getLogger(__name__)
+
+
+# socket = 'localhost:50051'
+# channel = grpc.insecure_channel(socket)
+# client = my_grpc.BasicExampleStub(channel)
+
+
+# def make_unary_request(request_string: str):
+#     log.info("Making Unary Request")
+#     pb_req = my_pb.BasicExampleRequest(action_name=request_string)
+#     log.info(f"Request - {pb_req}")
+#     u_res = client.UnaryApi(pb_req)
+#     log.info(f"Response - {u_res}")
+
+
+# def make_server_streaming_request(request_string: str):
+#     log.info("making Server Streaming Request")
+#     pb_req = my_pb.BasicExampleRequest(action_name=request_string)
+#     log.info(f"Request - {pb_req}")
+#     # for response in client.ServerStreamingAPI(pb_req):
+#     #     log.info(f"Response - {response}")
+#     ### Both works
+#     res = client.ServerStreamingApi(pb_req)
+#     for r in res:
+#         log.info(f"Response - {r}")
+
+
+# def make_client_streaming_request(request_string_list: list):
+#     log.info("Making Client Streaming request")
+#     pb_req_list = []
+#     for req_str in request_string_list:
+#         pb_req_list.append(my_pb.BasicExampleRequest(action_name=req_str))
+#     log.info(f"Request - {pb_req_list}")
+#     cs_res = client.ClientStreamingApi(pb_req_list.__iter__())
+#     log.info(f"Response - {cs_res}")
+
+
+# def make_bidirectional_stream_request(request_string_list: list):
+#     log.info("Making Bi-Directional Streaming request")
+#     pb_req_list = []
+#     for req_str in request_string_list:
+#         pb_req_list.append(my_pb.BasicExampleRequest(action_name=req_str))
+#     log.info(f"Request - {pb_req_list}")
+#     for bi_res in client.DuplexStreamingApi(pb_req_list.__iter__()):
+#         log.info(f"Response - {bi_res}")
+
 
 def run():
     # NOTE(gRPC Python Team): .close() is possible on a channel and should be
     # used in circumstances in which the with statement does not fit the needs
     # of the code.
-    print("Will try to greet world ...")
-    with grpc.insecure_channel('127.0.0.1:50051') as channel:
-        stub = helloworld_pb2_grpc.GreeterStub(channel)
-        response = stub.SayHello(helloworld_pb2.HelloRequest(name='you'))
-    print("Greeter client received: " + response.message)
+    # print("Will try to greet world ...")
+    # with grpc.insecure_channel('127.0.0.1:50051') as channel:
+    #     stub = helloworld_pb2_grpc.GreeterStub(channel)
+    #     response = stub.SayHello(helloworld_pb2.HelloRequest(name='you'))
+    # print("Greeter client received: " + response.message)
 
+    with grpc.insecure_channel('127.0.0.1:50051') as channel:
+        client = my_grpc.BasicExampleStub(channel)
+        # response = stub.SayHello(helloworld_pb2.HelloRequest(name='you'))
+        print("Making Unary Request")
+        pb_req = my_pb.BasicExampleRequest(action_name='some request string')
+        print(f"Request - {pb_req}")
+        u_res = client.UnaryApi(pb_req)
+        print(f"Response - {u_res}")
+
+
+    # make_server_streaming_request("Need stream response")
+
+    # make_client_streaming_request(["Client stream 1", "client stream 2", "Client Stream 3"])
+
+    # make_bidirectional_stream_request(["Bi-Directional req stream 1", "Bi-Directional req  stream 2"])
 
 if __name__ == '__main__':
     logging.basicConfig()
     run()
+
+
+# https://betterprogramming.pub/grpc-file-upload-and-download-in-python-910cc645bcf0
+# https://github.com/techunits?tab=repositories&q=grpc&type=&language=&sort=
--- app/grpc_server.py
+++ app/grpc_server.py
@@ -8,21 +8,61 @@ import grpc
 from protos.helloworld_pb2 import HelloReply
 from protos.helloworld_pb2_grpc import GreeterServicer, add_GreeterServicer_to_server
 
+from protos.basic_example_pb2 import BasicExampleResponse
+from protos.basic_example_pb2_grpc import BasicExampleServicer, add_BasicExampleServicer_to_server
+
 from logger import Logger
 
 log = Logger()
 
 
 class Greeter(GreeterServicer):
-
     def SayHello(self, request, context):
         return HelloReply(message='Hello, %s!' % request.name)
+
+
+class Four_Modes(BasicExampleServicer):
+    def UnaryApi(self, request, context):
+        log.info("Unary API")
+        log.info(f"Requests - {request}")
+        # print(dir(request))
+        # ['ByteSize', 'Clear', 'ClearExtension', 'ClearField', 'CopyFrom', 'DESCRIPTOR', 'DiscardUnknownFields', 'Extensions', 'FindInitializationErrors', 'FromString', 'HasExtension', 'HasField', 'IsInitialized', 'ListFields', 'MergeFrom', 'MergeFromString', 'ParseFromString', 'RegisterExtension', 'SerializePartialToString', 'SerializeToString', 'SetInParent', 'UnknownFields', 'WhichOneof', '_CheckCalledFromGeneratedFile', '_SetListener', '__class__', '__deepcopy__', '__delattr__', '__dir__', '__doc__', '__eq__', '__format__', '__ge__', '__getattribute__', '__getstate__', '__gt__', '__hash__', '__init__', '__init_subclass__', '__le__', '__lt__', '__module__', '__ne__', '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__setattr__', '__setstate__', '__sizeof__', '__slots__', '__str__', '__subclasshook__', '__unicode__', '_extensions_by_name', '_extensions_by_number', 'request_purpose']
+        res = BasicExampleResponse(message="Unary Response is here!")
+        log.info(f"Response - {res}")
+        return res
+
+    def ServerStreamingApi(self, request, context):
+        log.info("ServerStreaming API")
+        log.info(f"Request - {request}")
+        for i in range(10):
+            res = BasicExampleResponse(message=f"Stream Response : {i + 1}")
+            log.info(f"Response - {res}")
+            yield res
+
+    def ClientStreamingApi(self, request_iterator, context):
+        log.info("Client Streaming API")
+        for r in request_iterator:
+            log.info(f"Request - {r}")
+
+        res = BasicExampleResponse(message="Response for your stream requests")
+        log.info(f"Response - {res}")
+        return res
+
+    def BiDirectionalStreamingApi(self, request_iterator, context):
+        log.info("Bi-Directional Streaming API")
+        for r in request_iterator:
+            log.info(f"Request - {r}")
+            res = BasicExampleResponse(message=f"Response for your stream requests : {r.request_purpose}")
+            log.info(f"Response - {res}")
+            yield res
+
 
 
 def serve_grpc():
     port = '50051'
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     add_GreeterServicer_to_server(Greeter(), server)
+    add_BasicExampleServicer_to_server(Four_Modes(), server)
     server.add_insecure_port('[::]:' + port)
     server.start()
     log.info("Grpc server started", port=port)
